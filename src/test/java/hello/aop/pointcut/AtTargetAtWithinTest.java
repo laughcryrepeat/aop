a594b952378dbbd092274c3a145fe9595d98a42a
오현화 <meke@nate.com>
2023-08-13 22:30:35 +0900

    @target @within aspect

---
+++ src/test/java/hello/aop/pointcut/AtTargetAtWithinTest.java
@@ -0,0 +1,77 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.annotation.ClassAop;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import({AtTargetAtWithinTest.Config.class})
+@SpringBootTest
+public class AtTargetAtWithinTest {
+
+    @Autowired
+    Child child;
+
+    @Test
+    void success() {
+        log.info("child Proxy={}", child.getClass());
+        child.childMethod(); //부모, 자식 모두 있는 메서드
+        child.parentMethod(); //부모 클래스만 있는 메서드
+    }
+
+    static class Config {
+
+        @Bean
+        public Parent parent() {
+            return new Parent();
+        }
+        @Bean
+        public Child child() {
+            return new Child();
+        }
+        @Bean
+        public AtTargetAtWithinAspect atTargetAtWithinAspect() {
+            return new AtTargetAtWithinAspect();
+        }
+    }
+
+    static class Parent {
+        public void parentMethod(){} //부모에만 있는 메서드
+    }
+
+    @ClassAop
+    static class Child extends Parent {
+        public void childMethod(){}
+    }
+
+    // @args, @target 포인트컷 지시자는 단독으로 사용하면 오류 가능성.
+    // 스프링 컨테이너 로딩시점에 프록시 생성하여 모든 스프링 빈에 AOP를 적용하려고 함.
+    // 프록시가 있어야 실행시점에 판단함.
+    // 따라서 프록시 적용 대상을 축소하는 표현식과 함께 사용.
+
+    @Slf4j
+    @Aspect
+    static class AtTargetAtWithinAspect {
+
+        //@target: 인스턴스 기준으로 모든 메서드의 조인 포인트를 선정, 부모 타입의 메서드도 적용
+        @Around("execution(* hello.aop..*(..)) && @target(hello.aop.member.annotation.ClassAop)")
+        public Object atTarget(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[@target] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        //@within: 선택된 클래스 내부에 있는 메서드만 조인 포인트로 선정, 부모 타입의 메서드는 적용되지 않음
+        @Around("execution(* hello.aop..*(..)) && @within(hello.aop.member.annotation.ClassAop)")
+        public Object atWithin(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[@within] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+}
